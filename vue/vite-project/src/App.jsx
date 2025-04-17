4f5d9a8ec27712822f4d6889472df71703ee1a0b
mikez <zeldabakhos6@gmail.com>
2025-04-17 10:24:21 +0200

    LogIN

--- vue/vite-project/src/App.jsx
+++ vue/vite-project/src/App.jsx
@@ -2,7 +2,7 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 
 import CreateProduct from "./pages/CreateProduct";
 import HomePage from "./pages/HomePage";
-import LogInPage from "./pages/LoginPage";
+import LogInPage from "./pages/LogInPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import SignUpPage from "./pages/SignUpPage";
 
